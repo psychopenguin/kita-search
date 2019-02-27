ffba06199a4eee23724161c6b6915ded66ef2a3a
Fabio Brito d'Araujo e Oliveira <psychopenguin@gmail.com>
2019-02-27 10:18:18 +0100

    Changed to GO

--- kita.go
+++ kita.go
@@ -3,51 +3,43 @@ package main
 import "fmt"
 import "time"
 import "log"
-import "net/url"
+import "strings"
 import "github.com/gocolly/colly"
 
 const start string = "https://www.berlin.de/sen/jugend/familie-und-kinder/kindertagesbetreuung/kitas/verzeichnis/ListeKitas.aspx"
 
-func main(){
+func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.berlin.de"),
 		colly.Async(true),
 	)
-	
+
 	c.Limit(&colly.LimitRule{
-		DomainGlob: "*berlin.de*",
+		DomainGlob:  "*berlin.de*",
 		Parallelism: 3,
-		Delay: 1 * time.Second,
+		Delay:       1 * time.Second,
 		RandomDelay: 2 * time.Second,
 	})
-	
-	k := c.Clone()
 
-	c.OnRequest(func (r *colly.Request){
-		log.Println("Retrieving Kita list from:", r.URL.String())
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("Visiting:", r.URL.String())
 	})
 
 	c.OnHTML("#DataList_Kitas tr a", func(e *colly.HTMLElement) {
-		// URLs returned by this contains spaces and need to be encoded properly
-		raw_link := e.Request.AbsoluteURL(e.Attr("href"))
-		u, _ := url.Parse(raw_link)
-		u.RawQuery = url.QueryEscape(u.RawQuery)
-		fmt.Println(u.String())
-		k.Visit(u.String())
-		k.Wait()
+		//berlin.de website sends not escaped spaces in it href link
+		//this just replace the spaces with + signs to scape them
+		l := strings.Replace(e.Request.AbsoluteURL(e.Attr("href")), " ", "+", -1)
+		c.Visit(l)
 	})
-	
-	
-	k.OnHTML("#frmKitaDetailNeu", func(e *colly.HTMLElement) {
-		name := e.ChildText("#Allgemein")
+	c.OnHTML("#frmKitaDetailNeu", func(e *colly.HTMLElement) {
+		name := e.ChildText("#Allgemein h1")
 		fmt.Println(name)
 	})
 
-	k.OnError(func(r *colly.Response, err error){
-		log.Println(r.StatusCode, err)
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println(r.Request.URL.String(), r.StatusCode, err)
 	})
 
-	
 	c.Visit(start)
 	c.Wait()
 }
