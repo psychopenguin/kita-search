09bf681a822d2f76385a5aae11d3e4087d364c3d
Fabio Brito d'Araujo e Oliveira <psychopenguin@gmail.com>
2019-03-07 17:55:01 +0100

    added relationship

--- pkg/kita/kita.go
+++ pkg/kita/kita.go
@@ -2,9 +2,16 @@ package kita
 
 import "github.com/jinzhu/gorm"
 
+type District struct {
+	gorm.Model
+	Name  string `gorm:"UNIQUE"`
+	Kitas []Kita
+}
+
 type Kita struct {
 	gorm.Model
 	Name      string
 	Email     string
 	Permalink string `gorm:"UNIQUE"`
+	District  District
 }
