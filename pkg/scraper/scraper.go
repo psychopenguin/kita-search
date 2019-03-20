e46269b0d6fac01534ac3fe4ba5f9743ba15dbc7
Fabio Brito d'Araujo e Oliveira <psychopenguin@gmail.com>
2019-03-20 18:04:30 +0100

    removed db logic from scrapper

--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -20,26 +20,22 @@ import (
 	"time"
 
 	"github.com/gocolly/colly"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/psychopenguin/kita-search/pkg/kita"
 )
 
-func Scrape(url string) {
-	db, err := gorm.Open("sqlite3", "kita.db")
-	if err != nil {
-		panic("Failed to open db")
-	}
+const (
+	CSSPathKitaList    = "#DataList_Kitas tr a"
+	CSSPathKitaDetails = "#frmKitaDetailNeu"
+)
 
-	defer db.Close()
+func Scrape(url string) {
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("www.berlin.de"),
 		colly.Async(true),
 	)
 
 	c.Limit(&colly.LimitRule{
-		DomainGlob:  "*berlin.de*",
+		DomainGlob:  "*",
 		Parallelism: 3,
 		Delay:       1 * time.Second,
 		RandomDelay: 2 * time.Second,
@@ -49,18 +45,17 @@ func Scrape(url string) {
 		log.Println("Visiting:", r.URL.String())
 	})
 
-	c.OnHTML("#DataList_Kitas tr a", func(e *colly.HTMLElement) {
+	c.OnHTML(CSSPathKitaList, func(e *colly.HTMLElement) {
 		//berlin.de website sends not escaped spaces in it href link
 		//this just replace the spaces with + signs to scape them
 		l := strings.Replace(e.Request.AbsoluteURL(e.Attr("href")), " ", "+", -1)
 		c.Visit(l)
 	})
-	c.OnHTML("#frmKitaDetailNeu", func(e *colly.HTMLElement) {
+	c.OnHTML(CSSPathKitaDetails, func(e *colly.HTMLElement) {
 		var k kita.Kita
 		k.Permalink = e.Request.URL.String()
 		k.Name = e.ChildText("#Allgemein h1")
 		k.Email = e.ChildText("#HLinkEMail")
-		db.Create(&k)
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
